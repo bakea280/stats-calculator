97d72b7cebd58d5a370195eab00166f0e8a6222a
Baker_Aiden <bakea280@hsd.k12.or.us>
2024-05-22 10:27:25 -0700

    improvements

--- stats_calculator/app.py
+++ stats_calculator/app.py
@@ -16,54 +16,56 @@ from PyQt6.QtWidgets import (
 )
 
 from layout_colorwidget import Color
+from normalcdf import NormalCDF
 
-class NormalCDFTab(QWidget):
-    def __init__(self):
-        super().__init__()
+# class NormalCDFTab(QWidget):
+#     def __init__(self):
+#         super().__init__()
 
-        self.label1 = QLabel('Label 1:')
-        self.label2 = QLabel('Label 2:')
+#         self.label1 = QLabel('Label 1:')
+#         self.label2 = QLabel('Label 2:')
 
-        self.textbox1 = QLineEdit()
-        self.textbox2 = QLineEdit()
+#         self.textbox1 = QLineEdit()
+#         self.textbox2 = QLineEdit()
 
-        # self.textbox1.setSizePolicy(
-        #     QSizePolicy.Policy.Fixed,
-        #     QSizePolicy.Policy.Fixed,
-        # )
-        # self.textbox2.setSizePolicy(
-        #     QSizePolicy.Policy.Fixed,
-        #     QSizePolicy.Policy.Fixed,
-        # )
+#         # self.textbox1.setSizePolicy(
+#         #     QSizePolicy.Policy.Fixed,
+#         #     QSizePolicy.Policy.Fixed,
+#         # )
+#         # self.textbox2.setSizePolicy(
+#         #     QSizePolicy.Policy.Fixed,
+#         #     QSizePolicy.Policy.Fixed,
+#         # )
 
-        h_layout1 = QHBoxLayout()
-        h_layout1.addWidget(self.label1)
-        h_layout1.addWidget(self.textbox1)
+#         h_layout1 = QHBoxLayout()
+#         h_layout1.addWidget(self.label1)
+#         h_layout1.addWidget(self.textbox1)
 
-        h_layout2 = QHBoxLayout()
-        h_layout2.addWidget(self.label2)
-        h_layout2.addWidget(self.textbox2)
+#         h_layout2 = QHBoxLayout()
+#         h_layout2.addWidget(self.label2)
+#         h_layout2.addWidget(self.textbox2)
 
-        v_layout = QVBoxLayout()
-        v_layout.addLayout(h_layout1)
-        v_layout.addLayout(h_layout2)
+#         v_layout = QVBoxLayout()
+#         v_layout.addLayout(h_layout1)
+#         v_layout.addLayout(h_layout2)
 
-        v_layout.setSizeConstraint(QVBoxLayout.SizeConstraint.SetFixedSize)
+#         v_layout.setSizeConstraint(QVBoxLayout.SizeConstraint.SetFixedSize)
 
-        self.setLayout(v_layout)
+#         self.setLayout(v_layout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Statistics Calculator")
+        self.setMinimumSize(QSize(480, 270))
 
         tabs = QTabWidget()
         # tabs.setTabPosition(QTabWidget.TabPosition.West)
         # tabs.setMovable(True)
 
         
-        tabs.addTab(NormalCDFTab(), 'NormalCDF')
+        tabs.addTab(NormalCDF(), 'NormalCDF')
         
         for n, color in enumerate(["red", "green", "blue", "yellow"]):
             tabs.addTab(Color(color), color)
--- stats_calculator/normalcdf.py
+++ stats_calculator/normalcdf.py
@@ -14,13 +14,20 @@ from PyQt6.QtWidgets import (
     QSizePolicy,
     QLayout
 )
+from PyQt6.QtGui import QRegularExpressionValidator
+from PyQt6.QtCore import QRegularExpression
 
 class LabeledTextbox(QWidget):
-    def __init__(self, label_text, call_on_edit=False):
+    def __init__(self, label_text, call_on_edit=None, regex_filter=None):
         super().__init__()
 
         self.label = QLabel(label_text)
         self.textbox = QLineEdit()
+
+        if regex_filter:
+            regex = QRegularExpression(regex_filter)
+            validator = QRegularExpressionValidator(regex, self.textbox)
+            self.textbox.setValidator(validator)
 
         if call_on_edit:
             self.textbox.textEdited.connect(call_on_edit)
@@ -30,29 +37,37 @@ class LabeledTextbox(QWidget):
         layout.addWidget(self.textbox)
         self.setLayout(layout)
 
-        
+equation_regex = '[0-9\Q.+-*/x^(), \E]*' 
 
 class NormalCDF(QWidget):
     def __init__(self):
         super().__init__()
 
-        mean_box = LabeledTextbox('mean: ', self.mean_changed)
-        
+        mean_box = LabeledTextbox('mean (μ):', self.mean_changed, equation_regex)
+        stddev_box = LabeledTextbox('standard deviation (σ):', self.stddev_changed, equation_regex)
+
         layout = QVBoxLayout()
-        test = QLabel('bruhh')
-        test2 = QLabel('ok')
-        layout.addWidget(test)
         layout.addWidget(mean_box)
-        layout.addWidget(test2)
+        layout.addWidget(stddev_box)
+
+        
+        # test = QLabel('bruhh')
+        # test2 = QLabel('ok')
+        # layout.addWidget(test)
+        # layout.addWidget(test2)
 
         self.setLayout(layout)
     
     def mean_changed(self, text):
-        print(f'text edited to: {text}')
-    
-app = QApplication(sys.argv)
+        print(f'mean text edited to: {text}')
 
-window = NormalCDF()
-window.show()
+    def stddev_changed(self, text):
+        print(f'stddev text edited to: {text}')
 
-app.exec()
+if __name__ == '__main__': 
+    app = QApplication(sys.argv)
+
+    window = NormalCDF()
+    window.show()
+
+    app.exec()
